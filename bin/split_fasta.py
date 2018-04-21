587adcae51b0cc3e665883ccc467d65766db4aa8
Bradford Condon <bradford.condon@gmail.com>
2018-04-21 15:32:04 -0400

    splitter owrks

--- bin/split_fasta.py
+++ bin/split_fasta.py
@@ -31,9 +31,26 @@ def main(argv):
 
     bite_size = math.ceil(total_count / out_count)
 
-    print(bite_size)
+    print(str(total_count) + " input sequences.  Generating " + str(int(bite_size)) + " output files" )
+
+    placeholder = 1
+    output_file = input_file + "." + str(placeholder)
+
+    ofh = open(output_file, "w")
 
     fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
+    for fasta in fasta_sequences:
+        # check if we need a new output file.
+        if placeholder % 10000 == 0 :
+            print "Processing sequence " + str(placeholder)
+        if placeholder % bite_size == 0:
+            ofh.close()
+            output_file = input_file + "." + str(int(placeholder / bite_size))
+            ofh = open(output_file, "w")
+        name, sequence = fasta.id, str(fasta.seq)
+        ofh.write('>' + name + '\n' + sequence + '\n')
+        placeholder += 1
+    ofh.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
