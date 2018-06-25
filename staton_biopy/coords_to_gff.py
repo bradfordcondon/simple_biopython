e8778deca64915ae731284a6879629efedf3f8d8
Bradford Condon <bradford.condon@gmail.com>
2018-06-25 15:13:06 -0400

    parser now creates parent feature that individual alignments get associated with

--- staton_biopy/coords_to_gff.py
+++ staton_biopy/coords_to_gff.py
@@ -19,49 +19,66 @@ def convert_gff(argv):
 
             reference_index = parent.get(reference_id)
 
-            if !reference_index:
-                parent[reference_id] = {query_id: {"start": q_start, "stop": q_stop}}
+            if reference_index is None:
+                parent[reference_id] = {
+                    query_id: {"start": q_start, "end": q_end, "strand": strand, "sections": [line]}}
 
             else:
                 query_index = reference_index.get(query_id)
 
-                if !query_index:
-                    parent[reference_id][query_id] = {"start": q_start, "stop": q_stop}
+                if query_index is None:
+                    parent[reference_id][query_id] = {"start": q_start, "end": q_end, "strand": strand,
+                                                      "sections": [line]}
 
                 else:
                     current = parent[reference_id][query_id]
                     prev_start = current["start"]
                     prev_end = current["end"]
+                    sections = current["sections"]
                     if q_start < prev_start:
                         current["start"] = q_start
                     if q_end > prev_end:
                         current["end"] = prev_end
+                    sections.append(line)
+                    parent[reference_id][query_id] = {"start": q_start, "end": q_end, "strand": strand,
+                                                      "sections": sections}
 
+    for parent_id, query_package in parent.items():
+        for query_id, record in query_package.items():
+            parent_start = record["start"]
+            parent_end = record["end"]
+            strand = record["strand"]
+            attributes = "ID=" + reference_id + "; Name=" + reference_id + ";"
+            print_out(query_id, parent_start, parent_end, strand, attributes)
+            count = 1
 
-        datafile.seek(0)
+            sections = record["sections"]
 
-        for line in datafile:
-            parsed = parse_line(line)
-            query_id = parsed["query_id"]
-            reference_id = parsed["reference_id"]
-            q_start = parsed["q_start"]
-            q_end = parsed["q_end"]
-            strand = parsed["strand"]
+            for section in sections:
+                parsed = parse_line(section)
+                query_id = parsed["query_id"]
+                reference_id = parsed["reference_id"]
+                q_start = parsed["q_start"]
+                q_end = parsed["q_end"]
+                strand = parsed["strand"]
 
+                attributes = "Parent=" + reference_id + "; ID=" + reference_id + "_" + str(
+                    count) + "; Name=" + reference_id + "_" + str(count) + ";"
 
+                print_out(query_id, q_start, q_end, strand, attributes)
 
-
-
-    """After going first time, loop through again and print out parent info"""
-
-    attributes = "ID=" + reference_id + "; Name=" + reference_id + ";"
-    print_out(query_id, print_start, print_end, print_strand, attributes)
+                count += 1
 
 
 def print_out(query_id, start, end, strand, attributes):
     final = "\t"
+    type = "scaffold"
+
+    if attributes.find("Parent=") != -1:
+        type = "alignment"
+
     print final.join([query_id, "nucmer",
-                      "alignment", start,
+                      type, start,
                       end, ".",
                       strand, ".",
                       attributes
@@ -89,7 +106,9 @@ def parse_line(line):
         print_start = q_end
         print_end = q_start
 
-    return {"query_id": query_id, "reference_id": reference_id, "q_start": print_start, "q_end": print_end, "strand": print_strand}
+    return {"query_id": query_id, "reference_id": reference_id, "q_start": print_start, "q_end": print_end,
+            "strand": print_strand}
+
 
 if __name__ == "__main__":
     convert_gff(sys.argv[1:])
